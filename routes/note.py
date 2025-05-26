8206a8ef1fbed53a0dab279ebd678dc15e84a59e
makrand2222 <hiten@in>
2025-05-26 22:40:13 +0530

    [UPDATED] Create and retrieve APIs updated

--- routes/note.py
+++ routes/note.py
@@ -14,7 +14,15 @@ async def get_root(request: Request):
     docs = conn.notes.notes.find({})
     newDocs = []
     for doc in docs:
-        newDocs.append({"id": doc["_id"], "note": doc["note"]})
+        newDocs.append(
+            {
+                "id": doc["_id"],
+                "title": doc["title"],
+                "desc": doc["desc"],
+                "important": doc["important"] 
+                }
+            )
+    print("newDocs: ",newDocs)
     return templates.TemplateResponse(
         "index.html", {"request": request, "newDocs": newDocs}
     )
@@ -23,3 +31,11 @@ async def get_root(request: Request):
 @note.get('/items/{item_id}')
 def get_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@note.post('/')
+async def create_item(request: Request):
+    form = await request.form()
+    formDict = dict(form)
+    formDict["important"] = True if formDict.get("important") == "on" else False
+    note = conn.notes.notes.insert_one(formDict)
+    return {"success": True}
