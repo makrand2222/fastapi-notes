d0b11ab1a2ea5e8720ef370d18d38926c06026a0
makrand2222 <makrandmandhare@gmail.com>
2025-05-29 12:22:56 +0530

    [ADDED] adding .env file

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ import os
 
 from pymongo import MongoClient
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://makrandmandhare:f2uO4TUzUXeQ8TGH@fastapi.hdfdzwv.mongodb.net/notes")
+MONGO_URL = os.getenv("MONGO_URL", None)
 
 conn = MongoClient(
     MONGO_URL
